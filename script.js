e1e6ffdf6cf36e2558c1f819a28f45e5807f37f3
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-03-08 22:03:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //your code here
 
+const input = document.getElementById('newTodoInput');
+const btn = document.getElementById('addTodoBtn');
+const list = document.getElementById('todoList');
+
+
+
+btn.addEventListener('click', ()=>{
+	const valueF = input.value.trim();
+	if(valueF == ''){
+		alert("Please Enter a Valid Todo!")
+	}
+	const listItem = document.createElement('li');
+	listItem.innerHTML = `${valueF}`;
+
+	list.appendChild(listItem);
+})
